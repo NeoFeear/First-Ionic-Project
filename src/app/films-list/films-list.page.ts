0ec20770b498997cb6b2c9e7c4fca2c46260ab4b
Florian Martin <neofeear@gmail.com>
2021-12-14 11:26:24 +0100

    Ajout de la liste de films

--- src/app/films-list/films-list.page.ts
+++ src/app/films-list/films-list.page.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmsListPage implements OnInit {
 
+  films = [
+    {
+      id: 1,
+      title: 'titre 1',
+      img: '/assets/img/noimg.jpg',
+      param1: 'param 1',
+      param2: 'param 2'
+    },
+    {
+      id: 2,
+      title: 'titre 2',
+      img: '/assets/img/noimg.jpg',
+      param1: 'param 1',
+      param2: 'param 2'
+    },
+    {
+      id: 3,
+      title: 'titre 3',
+      img: '/assets/img/noimg.jpg',
+      param1: 'param 1',
+      param2: 'param 2'
+    },
+    {
+      id: 4,
+      title: 'titre 4',
+      img: '/assets/img/noimg.jpg',
+      param1: 'param 1',
+      param2: 'param 2'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
